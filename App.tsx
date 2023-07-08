bcd6092ec2d7a9dc03de429fadd01d97ae4195db
jonathan_baraza <jbaraza46@gmail.com>
2023-07-08 15:20:02 +0300

    loaded roboto themes

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
+import { useCallback } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 import Register from "./screens/auth/Register";
 import Welcome from "./screens/Welcome";
 
@@ -7,9 +8,34 @@ import Welcome from "./screens/Welcome";
 import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
 
+SplashScreen.preventAutoHideAsync();
+
 export default function App() {
+  const [isLoaded] = useFonts({
+    "mrt-thin": require("./assets/fonts/Montserrat/Montserrat-Thin.ttf"),
+    "mrt-mid": require("./assets/fonts/Montserrat/Montserrat-Medium.ttf"),
+    "mrt-semi": require("./assets/fonts/Montserrat/Montserrat-SemiBold.ttf"),
+    "mrt-bold": require("./assets/fonts/Montserrat/Montserrat-Bold.ttf"),
+    "mrt-xbold": require("./assets/fonts/Montserrat/Montserrat-ExtraBold.ttf"),
+    "rb-thin": require("./assets/fonts/Roboto/Roboto-Thin.ttf"),
+    "rb-mid": require("./assets/fonts/Roboto/Roboto-Medium.ttf"),
+    "rb-semi": require("./assets/fonts/Roboto/Roboto-Regular.ttf"),
+    "rb-bold": require("./assets/fonts/Roboto/Roboto-Bold.ttf"),
+    "rb-xbold": require("./assets/fonts/Roboto/Roboto-Black.ttf"),
+  });
+
+  const handleOnLayout = useCallback(async () => {
+    if (isLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [isLoaded]);
+
+  if (!isLoaded) {
+    return null;
+  }
+
   return (
-    <View className="flex-1">
+    <View className="flex-1" onLayout={handleOnLayout}>
       <Welcome />
     </View>
   );
