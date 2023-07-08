7f58e7150f4212b12b99869d94dc879870fd79fd
jonathan_baraza <jbaraza46@gmail.com>
2023-07-08 14:30:00 +0300

    pages imports

--- App.tsx
+++ App.tsx
@@ -1,22 +1,16 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import Register from "./screens/auth/Register";
+import Welcome from "./screens/Welcome";
+
+//setting up fonts
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text className="text-red-500">
-        Open up App.tsx to start working on your app!
-      </Text>
-      <StatusBar style="auto" />
+    <View className="flex-1">
+      <Welcome />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
