b25306f977f812cc6b039894a075724e20eb2ffb
jonathan_baraza <jbaraza46@gmail.com>
2023-07-09 20:45:59 +0300

    home status bar

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,12 +1,17 @@
-import { View, Text } from "react-native";
+import { View, Text, StatusBar, StyleSheet } from "react-native";
 import React from "react";
 
 const Home = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Home</Text>
     </View>
   );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    marginTop: StatusBar.currentHeight || 0,
+  },
+});
 export default Home;
